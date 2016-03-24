8a906f64e8c3f9befa2c48d07c456019edb68bb0
Dide van Berkel <didevanberkel@macbook-air-van-dide.local>
2016-03-24 20:27:16 +0100

    Initial Commit

---
+++ DevBootcamp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  DevBootcamp
+//
+//  Created by Dide van Berkel on 24-03-16.
+//  Copyright © 2016 Gary Grape Productions. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
